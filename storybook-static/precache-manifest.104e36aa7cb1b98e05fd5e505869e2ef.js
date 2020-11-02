321ebfd2c4bb1a25aa8e67991debb74a1030f568
unknown <namlong9012@gmail.com>
2020-11-02 15:29:27 +0700

    deploy storybook to firebase

---
+++ storybook-static/precache-manifest.104e36aa7cb1b98e05fd5e505869e2ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a396636417a403de9426c0b5fd3e6d91",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.7cae12e53ca204fee76f.bundle.js"
+  },
+  {
+    "url": "runtime~main.7cae12e53ca204fee76f.bundle.js"
+  },
+  {
+    "revision": "401fc078d292d60bed67",
+    "url": "static/css/vendors~main.2a86faee.chunk.css"
+  },
+  {
+    "url": "vendors~main.7cae12e53ca204fee76f.bundle.js"
+  },
+  {
+    "url": "vendors~main.7cae12e53ca204fee76f.bundle.js.LICENSE.txt"
+  }
+]);
